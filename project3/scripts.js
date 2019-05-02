3e191265b8f4613a4a045749457b2e76349fa2dc
Maggie Marino <mmarino4@nd.edu>
2019-05-02 12:46:36 -0400

    update parameters for fadein

--- project3/scripts.js
+++ project3/scripts.js
@@ -1,3 +1,11 @@
+
+
 AOS.init({
   easing: 'ease', // default easing for AOS animations
+
 })
+
+if (iosVersion >= 7) {
+    $(document).scroll(function() {
+        $('#background').css('background-position', '0px ' + $(document).scrollTop() + 'px');
+    });
